3c1978455c2f3e93bc3c20450af48b1e9e47f7e5
Kay Ousterhout <kay@quilt.io>
2017-09-29 18:30:43 -0700

    blueprint: Use a variable for replication factor

--- django-example.js
+++ django-example.js
@@ -4,6 +4,8 @@ const Django = require('./django.js');
 const haproxy = require('@quilt/haproxy');
 const Mongo = require('@quilt/mongo');
 
+const numReplicas = 3;
+
 // Create infrastructure.
 const deployment = createDeployment({});
 
@@ -12,15 +14,15 @@ const baseMachine = new Machine({
   // sshKeys: githubKeys("CHANGE_ME"), // Replace with your GitHub username
 });
 deployment.deploy(baseMachine.asMaster());
-deployment.deploy(baseMachine.asWorker().replicate(3));
+deployment.deploy(baseMachine.asWorker().replicate(numReplicas));
 
 // Create applications.
-const mongo = new Mongo(3);
+const mongo = new Mongo(numReplicas);
 mongo.deploy(deployment);
 
 // Create three Django replicas created from the "quilt/django-polls" image, and
 // connected to the mongo database.
-const django = new Django(3, 'quilt/django-polls', mongo);
+const django = new Django(numReplicas, 'quilt/django-polls', mongo);
 django.deploy(deployment);
 
 const proxy = haproxy.simpleLoadBalancer(django.containers);
