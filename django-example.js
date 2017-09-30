d30e7280a78c67dd2148e0a86955c039ee0d0c90
Kay Ousterhout <kay@quilt.io>
2017-09-29 18:26:09 -0700

    blueprint: Re-arrange example file
    This minor commit re-arranges the example file to group creating
    the infrastructure and creating the application files, and removes
    obvious comments.

--- django-example.js
+++ django-example.js
@@ -4,27 +4,24 @@ const Django = require('./django.js');
 const haproxy = require('@quilt/haproxy');
 const Mongo = require('@quilt/mongo');
 
-// Infrastructure
+// Create infrastructure.
 const deployment = createDeployment({});
 
 const baseMachine = new Machine({
   provider: 'Amazon',
   // sshKeys: githubKeys("CHANGE_ME"), // Replace with your GitHub username
 });
+deployment.deploy(baseMachine.asMaster());
+deployment.deploy(baseMachine.asWorker().replicate(3));
 
-// Applications
+// Create applications.
 const mongo = new Mongo(3);
 
-// Three Django replicas created from the "quilt/django-polls" image, and
+// Create three Django replicas created from the "quilt/django-polls" image, and
 // connected to the mongo database.
 const django = new Django(3, 'quilt/django-polls', mongo);
 
 const proxy = haproxy.simpleLoadBalancer(django.containers);
-
-// Connections
 proxy.allowFrom(publicInternet, 80);
 
-// Deployment
-deployment.deploy(baseMachine.asMaster());
-deployment.deploy(baseMachine.asWorker().replicate(3));
 deployment.deploy([django, mongo, proxy]);
